183e6b51d9086ca1ff5e17cdccfdc1da52540bd9
jankodedic7 <jankodedic7@gmail.com>
2018-08-18 11:21:24 +0200

    Problem: Cannot use object rest spread feature
    Solution: Install and setup babel-plugin-transform-object-rest-spread

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const AnotherComponent = ({...props}) => {
-// };
+const AnotherComponent = ({...props}) => {
+};
+
+const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
 
 class ExampleComponent extends React.Component {
   myMethod = () => {
