558e1ac8f1ea6241a8060669700723bdfbdbdbaa
jankodedic7 <jankodedic7@gmail.com>
2018-08-19 18:23:44 +0200

    Problem: Cannot reliably do further styling without normalized CSS.
    Solution: Install and setup normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'normalize.css/normalize.css';
 import './styles/components/Player.css';
 
 import Player from './components/Player';
