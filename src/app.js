1efc5221179a70f5966ea4346cb7adce01a8522f
jankodedic7 <jankodedic7@gmail.com>
2018-08-21 14:03:11 +0200

    Problem: Song dashboard navigation area missing.
    Solution: Add the search bar and the 'Add Song' button.

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ import './styles/styles.scss';
 
 import Header from './components/Header';
 import Player from './components/Player';
+import SongDashboardPage from './components/SongDashboardPage';
 
 const App = () => (
   <div>
     <Header />
+    <SongDashboardPage />
     <Player />
   </div>
 );
---
+++ src/components/SongDashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="song-dashboard">
+    <div className="song-dashboard__nav">
+      <input type="text" placeholder="Search for songs or artists" />
+      <button>Add Song</button>
+    </div>
+  </div>
+);
