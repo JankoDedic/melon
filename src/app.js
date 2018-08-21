c5040f284e8177ae9a5e29694117f07682163eb9
jankodedic7 <jankodedic7@gmail.com>
2018-08-21 11:51:36 +0200

    Problem: No header for the page.
    Solution: Create a new Header component containing the logo and logout button.
    Render the Header element.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+import Header from './components/Header';
 import Player from './components/Player';
 
 const App = () => (
-  <Player />
+  <div>
+    <Header />
+    <Player />
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div className="header">
+    <div className="header__logo">
+    </div>
+    <button className="header__logout">Logout</button>
+  </div>
+);
