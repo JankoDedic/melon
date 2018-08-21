b03a2e20edd9b11ced1c6f738dfde287b11e265b
jankodedic7 <jankodedic7@gmail.com>
2018-08-21 13:24:40 +0200

    Problem: No image in the header logo.
    Solution: Add the image to public/images, align it and make sure it fits in
    with the background color.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ export default () => (
   <div className="header-container">
     <div className="header">
       <div className="header__logo">
+        <div className="header__logo__image">
+        </div>
         Melon
       </div>
       <button className="header__logout">Logout</button>
