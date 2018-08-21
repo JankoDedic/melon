cb74ee3d8b5a4f0ec8dc91215bfe8c618e76e838
jankodedic7 <jankodedic7@gmail.com>
2018-08-21 11:06:17 +0200

    Problem: No master styles.scss file. player.scss is not a partial.
    Solution: Create the master styles.scss file, make the _player partial and
    import just the master file in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'normalize.css/normalize.css';
-import './styles/components/player.scss';
+import './styles/styles.scss';
 
 import Player from './components/Player';
 
