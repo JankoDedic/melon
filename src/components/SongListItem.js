b7631cf32d985e4ac8ef0c0f43c46bb1a2ac7541
jankodedic7 <jankodedic7@gmail.com>
2018-08-22 12:59:12 +0200

    Problem: Clicking on a SongListItem does not set that song as nowPlayingSong.
    Solution: Change SongListItem wrapper div to a, so you get access to onClick.
    Make onClick handler dispatch the setNowPlayingSong action.

--- src/components/SongListItem.js
+++ src/components/SongListItem.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default ({ song }) => (
-  <div className="song-list-item">
+import { setNowPlayingSong } from '../actions/nowPlayingSong';
+
+export const SongListItem = ({ song, setNowPlayingSong }) => (
+  <a className="song-list-item" onClick={setNowPlayingSong}>
     <div className="song-list-item__title">{song.title}</div>
     <div className="song-list-item__artists">{song.artists}</div>
     <div className="song-list-item__edit-button">
       <button>Edit</button>
     </div>
-  </div>
+  </a>
 );
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  setNowPlayingSong: (song) => dispatch(setNowPlayingSong(ownProps.song)),
+});
+
+export default connect(undefined, mapDispatchToProps)(SongListItem);
