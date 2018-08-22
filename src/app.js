a27a2c23217eedfca5495c30f155bf4b6ed93079
jankodedic7 <jankodedic7@gmail.com>
2018-08-22 10:05:23 +0200

    Problem: No store.
    Solution: Use redux to create a simple store.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -7,6 +8,18 @@ import './styles/styles.scss';
 import Header from './components/Header';
 import Player from './components/Player';
 import SongDashboardPage from './components/SongDashboardPage';
+
+const defaultSongs = [
+  { title: 'This Is What You Came For', artists: 'Calvin Harris, Rihanna' },
+  { title: 'My Way', artists: 'Frank Sinatra' }
+];
+
+const songsReducer = (songs, action) => {
+  return songs;
+};
+
+const store = createStore(songsReducer, defaultSongs);
+console.log(store.getState());
 
 const App = () => (
   <div>
