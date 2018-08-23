bb51b9cca4a7a587ac91f7aadc88c5fcc3e570f0
jankodedic7 <jankodedic7@gmail.com>
2018-08-23 16:19:03 +0200

    Problem: Searching or adding a song while playing halts the playback.
    Solution: There was a bug in the nowPlayingSong reducer, which was returning
    some default state in default: case, instead of the passed-in state.

--- src/reducers/nowPlayingSong.js
+++ src/reducers/nowPlayingSong.js
@@ -1,12 +1,8 @@
-export default (nowPlayingSong, action) => {
+export default (nowPlayingSong = {}, action) => {
   switch (action.type) {
     case 'SET_NOW_PLAYING_SONG':
       return action.song;
     default:
-      return {
-        title: '',
-        artists: '',
-        url: '',
-      };
+      return nowPlayingSong;
   }
 };
