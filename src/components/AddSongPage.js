47a409b783894516cd4ee55d70f28ef719467a42
jankodedic7 <jankodedic7@gmail.com>
2018-08-23 10:54:24 +0200

    Problem: Add Song page does not have a header nor form.
    Solution: Add them.

--- src/components/AddSongPage.js
+++ src/components/AddSongPage.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 export default () => (
   <div>
-    Add Song Page
+    <div className="add-song__header">
+      Add Song
+    </div>
+    <form className="add-song__form">
+      <input
+        type="text"
+        placeholder="Title"
+        autoFocus
+      />
+      <input
+        type="text"
+        placeholder="Artists"
+      />
+      <input
+        type="text"
+        placeholder="URL (YouTube, Soundcloud...)"
+      />
+      <button className="primary-button">Save Song</button>
+    </form>
   </div>
 );
