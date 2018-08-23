7f1ba12a65fe892533d907281afe43a4d8dc5343
jankodedic7 <jankodedic7@gmail.com>
2018-08-23 17:12:04 +0200

    Problem: EditSongPage will have almost the same code as AddSongPage.
    Solution: Split the code into a SongForm with a formTitle and onSubmit props.

--- src/components/AddSongPage.js
+++ src/components/AddSongPage.js
@@ -1,81 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ReactPlayer from 'react-player';
 
+import SongForm from './SongForm';
 import { addSong } from '../actions/songs';
 
-export class AddSongPage extends React.Component {
-  state = {
-    title: '',
-    artists: '',
-    url: '',
-    error: '',
-  };
-  handleTitleChange = (e) => {
-    this.setState({
-      title: e.target.value,
-    });
-  }
-  handleArtistsChange = (e) => {
-    this.setState({
-      artists: e.target.value,
-    });
-  }
-  handleURLChange = (e) => {
-    this.setState({
-      url: e.target.value,
-    });
-  }
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { title, artists, url } = this.state;
-    const isTitleValid = title.length > 0;
-    const isURLValid = ReactPlayer.canPlay(url);
-    if (!isTitleValid) {
-      this.setState({ error: 'Invalid title.' });
-    } else if (!isURLValid) {
-      this.setState({ error: 'Invalid URL. Melon cannot play this media.' });
-    } else {
-      this.props.addSong(title, artists, url);
-      this.props.history.push('/dashboard');
-    }
+class AddSongPage extends React.Component {
+  handleSubmit = (song) => {
+    this.props.addSong(song);
+    this.props.history.push('/dashboard');
   }
   render() {
     return (
-      <div>
-        <div className="add-song__header">
-          Add Song
-        </div>
-        {this.state.error && <p className="add-song__error">{this.state.error}</p>}
-        <form className="add-song__form" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="Title"
-            autoFocus
-            value={this.state.title}
-            onChange={this.handleTitleChange}
-          />
-          <input
-            type="text"
-            placeholder="Artists"
-            value={this.state.artists}
-            onChange={this.handleArtistsChange}
-          />
-          <input
-            type="text"
-            placeholder="URL (YouTube, SoundCloud...)"
-            value={this.state.url}
-            onChange={this.handleURLChange}
-          />
-          <button className="primary-button">Save Song</button>
-        </form>
-      </div>
+      <SongForm formTitle="Add Song" onSubmit={this.handleSubmit} />
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  addSong: (title, artists, url) => dispatch(addSong({ title, artists, url })),
-})
+  addSong: (song) => dispatch(addSong(song)),
+});
 
 export default connect(undefined, mapDispatchToProps)(AddSongPage);
---
+++ src/components/SongForm.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+export default class SongForm extends React.Component {
+  state = {
+    title: '',
+    artists: '',
+    url: '',
+    error: '',
+  };
+  handleTitleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+  handleArtistsChange = (e) => {
+    this.setState({
+      artists: e.target.value,
+    });
+  }
+  handleURLChange = (e) => {
+    this.setState({
+      url: e.target.value,
+    });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { title, artists, url } = this.state;
+    const isTitleValid = title.length > 0;
+    const isURLValid = ReactPlayer.canPlay(url);
+    if (!isTitleValid) {
+      this.setState({ error: 'Invalid title.' });
+    } else if (!isURLValid) {
+      this.setState({ error: 'Invalid URL. Melon cannot play this media.' });
+    } else {
+      this.props.onSubmit({ title, artists, url });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="add-song__header">
+          {this.props.formTitle}
+        </div>
+        {this.state.error && <p className="add-song__error">{this.state.error}</p>}
+        <form className="add-song__form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Title"
+            autoFocus
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+          />
+          <input
+            type="text"
+            placeholder="Artists"
+            value={this.state.artists}
+            onChange={this.handleArtistsChange}
+          />
+          <input
+            type="text"
+            placeholder="URL (YouTube, SoundCloud...)"
+            value={this.state.url}
+            onChange={this.handleURLChange}
+          />
+          <button className="primary-button">Save Song</button>
+        </form>
+      </div>
+    );
+  }
+}
+
