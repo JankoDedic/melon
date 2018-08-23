5a9df04f5207f3b4740c92efde925f53fa44df62
jankodedic7 <jankodedic7@gmail.com>
2018-08-23 15:16:45 +0200

    Problem: AddSongPage is a stateless functional component, but it needs to be a controlled component.
    Solution: Turn it into a class component.

--- src/components/AddSongPage.js
+++ src/components/AddSongPage.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
-export default () => (
-  <div>
-    <div className="add-song__header">
-      Add Song
-    </div>
-    <form className="add-song__form">
-      <input
-        type="text"
-        placeholder="Title"
-        autoFocus
-      />
-      <input
-        type="text"
-        placeholder="Artists"
-      />
-      <input
-        type="text"
-        placeholder="URL (YouTube, SoundCloud...)"
-      />
-      <button className="primary-button">Save Song</button>
-    </form>
-  </div>
-);
+export class AddSongPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="add-song__header">
+          Add Song
+        </div>
+        <form className="add-song__form">
+          <input
+            type="text"
+            placeholder="Title"
+            autoFocus
+          />
+          <input
+            type="text"
+            placeholder="Artists"
+          />
+          <input
+            type="text"
+            placeholder="URL (YouTube, SoundCloud...)"
+          />
+          <button className="primary-button">Save Song</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddSongPage;
