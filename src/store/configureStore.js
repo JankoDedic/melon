8d883edb3648eb1f3761e63453ba457a09cd9124
jankodedic7 <jankodedic7@gmail.com>
2018-08-24 13:31:25 +0200

    Problem: Cannot dispatch asynchronous Redux actions.
    Solution: Install and setup redux-thunk.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,19 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import filtersReducer from '../reducers/filters';
 import songsReducer from '../reducers/songs';
 import nowPlayingSongReducer from '../reducers/nowPlayingSong';
 
 export default () => {
-  const store = createStore(combineReducers({
-    songs: songsReducer,
-    filters: filtersReducer,
-    nowPlayingSong: nowPlayingSongReducer,
-  }));
+  const store = createStore(
+    combineReducers({
+      songs: songsReducer,
+      filters: filtersReducer,
+      nowPlayingSong: nowPlayingSongReducer,
+    }),
+    applyMiddleware(thunk)
+  );
   return store;
 };
 
