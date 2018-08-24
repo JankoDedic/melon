a26f0c4c66ded7bbf39b1e790b30cf6be12bf843
jankodedic7 <jankodedic7@gmail.com>
2018-08-24 16:52:57 +0200

    Problem: Cannot login with a Google account.
    Solution: Set up a Google Auth Provider and hook up the startLogin action to
    the Login button.

--- src/app.js
+++ src/app.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { startSetSongs } from './actions/songs';
-import { setTextFilter } from './actions/filters';
-import { setNowPlayingSong } from './actions/nowPlayingSong';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import configureStore from './store/configureStore';
-
+import { startSetSongs } from './actions/songs';
 import AppRouter from './routers/AppRouter';
-import Header from './components/Header';
 import Player from './components/Player';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -29,3 +25,11 @@ const App = (
 ReactDOM.render(App, document.getElementById('app'));
 
 store.dispatch(startSetSongs());
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('login');
+  } else {
+    console.log('logout');
+  }
+});
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-export default () => (
+import { startLogin } from '../actions/auth';
+
+export const Header = ({ startLogin }) => (
   <div className="header-container">
     <div className="header">
       <Link className="header__logo" to="/dashboard">
         <div className="header__logo__image"></div>
         Melon
       </Link>
-      <button className="header__logout">Logout</button>
+      <button className="header__logout" onClick={startLogin}>Login</button>
     </div>
   </div>
 );
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin()),
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
