565e2cacb0033e7a5b052647ed09a43f2dfe2f19
jankodedic7 <jankodedic7@gmail.com>
2018-08-26 17:03:23 +0200

    Problem: LoginPage does not contain the Logo and slogan.
    Solution: Add them.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Logo from './Logo';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
   <div>
+    <Logo />
+    <div>
+      One music collection. Any stream.
+    </div>
     <button onClick={startLogin}>Login with Google</button>
   </div>
 );
