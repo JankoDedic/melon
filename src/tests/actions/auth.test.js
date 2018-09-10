ef3b8d42d4827bdf1c75f12ef2aefee0850817c5
jankodedic7 <jankodedic7@gmail.com>
2018-09-10 08:32:13 +0200

    Problem: auth action generator tests not utilizing Jest's describe/it
    Solution: Utilize them.

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,19 +1,24 @@
 import { login, logout } from '../../actions/auth';
 
-test('login action gets generated', () => {
-  const uid = 'sample uid';
-  const action = login(uid);
-  expect(action).toEqual({
-    type: 'LOGIN',
-    uid
+describe('login action generator', () => {
+  describe('given a uid string', () => {
+    const uid = 'sample uid';
+
+    it('generates a valid action', () => {
+      const action = login(uid);
+      expect(action).toEqual({
+        type: 'LOGIN',
+        uid
+      });
+    });
   });
 });
 
-test('logout action gets generated', () => {
-  const action = logout();
-  expect(action).toEqual({
-    type: 'LOGOUT'
+describe('logout action generator', () => {
+  it('generates a valid action', () => {
+    const action = logout();
+    expect(action).toEqual({
+      type: 'LOGOUT'
+    });
   });
 });
-
-
