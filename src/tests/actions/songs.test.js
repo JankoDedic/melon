b1586d400dcec7d645cdb92413edaa8bfcc5375c
jankodedic7 <jankodedic7@gmail.com>
2018-09-10 11:29:23 +0200

    Problem: songs actions tests not organized.
    Solution: Use describe blocks to organize similar tests, and minimize
    database resets.

--- src/tests/actions/songs.test.js
+++ src/tests/actions/songs.test.js
@@ -19,136 +19,202 @@ const defaultAuthState = {
   }
 };
 
-beforeEach((done) => {
-  const songsData = {};
-  songs.forEach(({ id, ...rest }) => {
-    songsData[id] = { ...rest };
-  });
-  database.ref(`users/${uid}/songs`).set(songsData).then(() => done());
-});
+describe('songs action generators', () => {
+  describe('set songs action generator', () => {
+    it('generates a valid action', () => {
+      const action = setSongs(songs);
 
-test('action for setting songs gets generated', () => {
-  const action = setSongs(songs);
-  expect(action).toEqual({
-    type: 'SET_SONGS',
-    songs
-  });
-});
-
-test('action for setting songs gets generated and dispatched async', (done) => {
-  const store = createMockStore(defaultAuthState);
-  const action = startSetSongs();
-  store.dispatch(action).then(() => {
-    const actions = store.getActions();
-    expect(actions[0]).toEqual({
-      type: 'SET_SONGS',
-      songs
+      expect(action).toEqual({
+        type: 'SET_SONGS',
+        songs
+      });
     });
-    done();
+  });
+
+  describe('clear songs action generator', () => {
+    it('generates a valid action', () => {
+      const action = clearSongs();
+
+      expect(action).toEqual({
+        type: 'CLEAR_SONGS'
+      });
+    });
+  });
+
+  describe('add song action generator', () => {
+    it('generates a valid action', () => {
+      const action = addSong(songs[0]);
+
+      expect(action).toEqual({
+        type: 'ADD_SONG',
+        song: songs[0]
+      });
+    });
+  });
+
+  describe('edit song action generator', () => {
+    describe('given a song id and updates', () => {
+      const id = 'sample id';
+      const updates = {
+        title: 'new title',
+        url: 'new url'
+      };
+
+      it('generates a valid action', () => {
+        const action = editSong(id, updates);
+
+        expect(action).toEqual({
+          type: 'EDIT_SONG',
+          id,
+          updates
+        });
+      });
+    });
+  });
+
+  describe('remove song action generator', () => {
+    describe('given a song id', () => {
+      const id = 'sample id';
+
+      it('generates a valid action', () => {
+        const action = removeSong(id);
+
+        expect(action).toEqual({
+          type: 'REMOVE_SONG',
+          id
+        });
+      });
+    });
   });
 });
 
-test('action for clearing songs gets generated', () => {
-  const action = clearSongs();
-  expect(action).toEqual({
-    type: 'CLEAR_SONGS'
-  });
-});
-
-test('action for adding a song gets generated', () => {
-  const action = addSong(songs[0]);
-  expect(action).toEqual({
-    type: 'ADD_SONG',
-    song: songs[0]
-  });
-});
-
-test('action for adding a song gets generated and dispatched async', (done) => {
-  const store = createMockStore(defaultAuthState);
-  const newSong = {
-    title: 'example title',
-    artists: 'some artists',
-    url: 'some random url'
+describe('songs async action generators', () => {
+  let store;
+  const setupStoreAndDatabase = () => {
+    store = createMockStore(defaultAuthState);
+    const songsData = {};
+    songs.forEach(({ id, ...rest }) => {
+      songsData[id] = { ...rest };
+    });
+    return database.ref(`users/${uid}/songs`).set(songsData);
   };
-  const action = startAddSong(newSong);
-  store.dispatch(action).then(() => {
-    const actions = store.getActions();
-    expect(actions[0]).toEqual({
-      type: 'ADD_SONG',
-      song: {
-        id: expect.any(String),
-        ...newSong
-      }
-    });
-    return database.ref(`users/${uid}/songs/${actions[0].song.id}`).once('value');
-  }).then((snapshot) => {
-    expect(snapshot.val()).toEqual(newSong);
-    done();
-  });
-});
 
-test('action for editing a song gets generated', () => {
-  const id = 'sample id';
-  const updates = {
-    title: 'new title',
-    url: 'new url'
-  };
-  const action = editSong(id, updates);
-  expect(action).toEqual({
-    type: 'EDIT_SONG',
-    id,
-    updates
-  });
-});
+  describe('start set songs action generator', () => {
+    beforeAll(setupStoreAndDatabase);
 
-test('action for editing a song gets generated and dispatched async', (done) => {
-  const store = createMockStore(defaultAuthState);
-  const updates = {
-    title: 'updated title',
-    artists: 'updated artists',
-    url: 'updated url'
-  };
-  const action = startEditSong(songs[1].id, updates);
-  store.dispatch(action).then(() => {
-    const actions = store.getActions();
-    expect(actions[0]).toEqual({
-      type: 'EDIT_SONG',
-      id: songs[1].id,
-      updates
-    });
-    return database.ref(`users/${uid}/songs/${actions[0].id}`).once('value');
-  }).then((snapshot) => {
-    const { id, ...rest } = songs[1];
-    expect(snapshot.val()).toEqual({
-      ...rest,
-      ...updates
-    });
-    done();
-  });
-});
+    it('dispatches a valid set songs action', () => {
+      const action = startSetSongs();
 
-test('action for removing a song gets generated', () => {
-  const id = 'sample id';
-  const action = removeSong(id);
-  expect(action).toEqual({
-    type: 'REMOVE_SONG',
-    id
-  });
-});
-
-test('action for removing a song gets generated and dispatched async', (done) => {
-  const store = createMockStore(defaultAuthState);
-  const id = songs[2].id;
-  const action = startRemoveSong(id);
-  store.dispatch(action).then(() => {
-    const actions = store.getActions();
-    expect(actions[0]).toEqual({
-      type: 'REMOVE_SONG',
-      id
+      return store.dispatch(action).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+          type: 'SET_SONGS',
+          songs
+        });
+      });
     });
-    return database.ref(`users/${uid}/songs/${songs[2].id}`).once('value');
-  }).then((snapshot) => {
-    expect(snapshot.val()).toBeFalsy();
-    done();
+  });
+
+  describe('start add song action generator', () => {
+    describe('given a new song', () => {
+      const newSong = {
+        title: 'example title',
+        artists: 'some artists',
+        url: 'some random url'
+      };
+
+      beforeAll(setupStoreAndDatabase);
+
+      let songID;
+      it('dispatches a valid add song action', () => {
+        const action = startAddSong(newSong);
+
+        return store.dispatch(action).then(() => {
+          const actions = store.getActions();
+          expect(actions[0]).toEqual({
+            type: 'ADD_SONG',
+            song: {
+              id: expect.any(String),
+              ...newSong
+            }
+          });
+          songID = actions[0].song.id;
+        });
+      });
+
+      it('adds a song to the database upon being dispatched', () => {
+        return database.ref(`users/${uid}/songs/${songID}`)
+          .once('value')
+          .then((snapshot) => {
+            expect(snapshot.val()).toEqual(newSong);
+          });
+      });
+    });
+  });
+
+  describe('start edit song action generator', () => {
+    describe('given the song id and updates', () => {
+      const id = songs[1].id;
+      const updates = {
+        title: 'updated title',
+        artists: 'updated artists',
+        url: 'updated url'
+      };
+
+      beforeAll(setupStoreAndDatabase);
+
+      it('dispatches a valid edit song action', () => {
+        const action = startEditSong(id, updates);
+
+        return store.dispatch(action).then(() => {
+          const actions = store.getActions();
+          expect(actions[0]).toEqual({
+            type: 'EDIT_SONG',
+            id,
+            updates
+          });
+        });
+      });
+
+      it('edits a song in the database upon being dispatched', () => {
+        return database.ref(`users/${uid}/songs/${id}`)
+          .once('value')
+          .then((snapshot) => {
+            const { id, ...rest } = songs[1];
+            expect(snapshot.val()).toEqual({
+              ...rest,
+              ...updates
+            });
+          });
+      });
+    });
+  });
+
+  describe('start remove song action generator', () => {
+    describe('given a song id', () => {
+      const id = songs[2].id;
+
+      beforeAll(setupStoreAndDatabase);
+
+      it('dispatches a valid remove song action', () => {
+        const action = startRemoveSong(id);
+
+        store.dispatch(action).then(() => {
+          const actions = store.getActions();
+          expect(actions[0]).toEqual({
+            type: 'REMOVE_SONG',
+            id
+          });
+        });
+      });
+
+      it('removes a song from the database upon being dispatched', () => {
+        return database.ref(`users/${uid}/songs/${id}`)
+          .once('value')
+          .then((snapshot) => {
+            expect(snapshot.val()).toBeFalsy();
+          });
+      });
+    });
   });
 });
