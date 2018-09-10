80a5cc58294df2628366218579656ad09d35507a
jankodedic7 <jankodedic7@gmail.com>
2018-09-10 11:55:37 +0200

    Problem: songs selector tests not structured.
    Solution: Organize them.

--- src/tests/selectors/songs.test.js
+++ src/tests/selectors/songs.test.js
@@ -1,10 +1,14 @@
 import songsSelector from '../../selectors/songs';
 import songs from '../fixtures/songs';
 
-test('songs get filtered/selected by text', () => {
+describe('given a text filter', () => {
   const filters = {
     text: 'muse'
   };
-  const selectedSongs = songsSelector(songs, filters);
-  expect(selectedSongs).toEqual([songs[0], songs[1]]);
+
+  it('selects songs containing that text', () => {
+    const selectedSongs = songsSelector(songs, filters);
+
+    expect(selectedSongs).toEqual([songs[0], songs[1]]);
+  });
 });
