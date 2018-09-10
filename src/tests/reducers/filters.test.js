ddbb20b6129402be1206470c5fdb67f436b74a74
jankodedic7 <jankodedic7@gmail.com>
2018-09-10 11:40:29 +0200

    Problem: filters reducer tests not organized.
    Solution: Use 'describe' and 'it' blocks.

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,11 +1,16 @@
 import filtersReducer from '../../reducers/filters';
 import { setTextFilter } from '../../actions/filters';
 
-test('text filter gets set in the next state', () => {
+describe('given an empty state and set text filter action', () => {
   const state = {};
   const textFilter = 'hello';
   const action = setTextFilter(textFilter);
-  expect(filtersReducer(state, action)).toEqual({
-    text: textFilter
+
+  it('sets the text filter', () => {
+    const newState = filtersReducer(state, action);
+
+    expect(newState).toEqual({
+      text: textFilter
+    });
   });
 });
