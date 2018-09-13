31e569de8af7fab33ab573a1c01683061e1f7bb6
jankodedic7 <jankodedic7@gmail.com>
2018-09-13 15:21:01 +0200

    Problem: Logo snapshot test text.
    Solution: Fix it to be consistent with SongForm.

--- src/tests/components/Logo.test.js
+++ src/tests/components/Logo.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import { Logo } from '../../components/Logo';
 
-test('Logo gets rendered properly', () => {
+test('Logo snapshot', () => {
   const wrapper = shallow(<Logo />);
   expect(wrapper).toMatchSnapshot();
 });
